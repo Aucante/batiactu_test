910dffc0e965e00e3fd9e2415a334e867aed4718
Aucante <sebastien.aucante@gmail.com>
2022-08-13 15:32:07 +0200

    apicontactcontroller

--- src/Controller/ApiContactController.php
+++ src/Controller/ApiContactController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/api/contact", name="api_contact_")
@@ -18,7 +22,7 @@ class ApiContactController extends AbstractController
      */
     public function index(
         ContactRepository $contactRepository
-    ): Response
+    ): JsonResponse
     {
         return $this->json($contactRepository->findAll(), 200, [], ['groups' => 'contact:read']);
     }
@@ -28,8 +32,30 @@ class ApiContactController extends AbstractController
      */
     public function item(
         Contact $contact
-    ): Response
+    ): JsonResponse
     {
         return $this->json($contact, 200, [], ['groups' => 'contact:read']);
+    }
+
+    /**
+     * @Route(name="post", methods={"POST"})
+     */
+    public function post(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer
+    ): JsonResponse
+    {
+
+        $contact = $serializer->deserialize($request->getContent(), Contact::class, 'json');
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            $serializer->serialize($contact, "json", ["groups" => 'contact:read']),
+            JsonResponse::HTTP_CREATED,
+            [],
+            true
+        );
     }
 }
